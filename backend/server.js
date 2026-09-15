317cd6d1d7d20be4d38527faec48b643cadda729
CeloMee <madean0662@gmail.com>
2026-09-15 09:52:36 -0300

    feat: cria tela de login

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor Sentinela rodando em http://localhost:${PORT}`);
+});
